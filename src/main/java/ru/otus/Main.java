5fea4bb3e92f6db8a9c35c9e7baef0e7f690d548
saroff <tarasoff154@gmail.com>
2019-02-08 19:52:13 +0300

    Spring 16

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -1,14 +1,24 @@
 package ru.otus;
 
-
+import com.sun.tools.javac.util.List;
 import io.reactivex.Observable;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class Main {
 
     public static void main(String[] args) {
-        Observable.range(20, 5)
-                // TODO: add code here
+        List<String> words = List.of(
+                "the",
+                "quick",
+                "brown",
+                "fox",
+                "jumped",
+                "over",
+                "the",
+                "lazy",
+                "dog");
+    
+        Observable.just(words)
                 .subscribe(System.out::println);
     }
 }
